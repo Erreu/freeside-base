40a6ffa02d30786a147897e886169c3855dc3c0e
ivan <ivan>
2003-07-17 16:02:04 +0000

    one last tiny bugfix for the retry_realtime functionality

--- FS/FS/cust_main.pm
+++ FS/FS/cust_main.pm
@@ -1394,7 +1394,7 @@ sub retry_realtime {
         grep {
                #$_->part_bill_event->plan eq 'realtime-card'
                $_->part_bill_event->eventcode =~
-                   /\$cust_bill\->realtime_(card|ach|lec)$/
+                   /\$cust_bill\->realtime_(card|ach|lec)/
                  && $_->status eq 'done'
                  && $_->statustext
              }
