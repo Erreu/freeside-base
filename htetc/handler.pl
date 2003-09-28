308b95cc6fbc442938bc65a002fbab9109ff09d2
ivan <ivan>
2003-09-28 02:36:21 +0000

    add upload of batch result from TD Canada Trust

--- htetc/handler.pl
+++ htetc/handler.pl
@@ -66,6 +66,7 @@ sub handler
       #use CGI::Carp qw(fatalsToBrowser);
       use Date::Format;
       use Date::Parse;
+      use File::Basename;
       use Tie::IxHash;
       use HTML::Entities;
       use IO::Handle;
