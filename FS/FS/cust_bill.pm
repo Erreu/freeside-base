92cf674731966846021279e4b422e7f6fc12a70d
ivan <ivan>
2003-10-02 14:20:24 +0000

    add emaildecline-exclude config option

--- FS/FS/cust_bill.pm
+++ FS/FS/cust_bill.pm
@@ -860,6 +860,8 @@ sub realtime_bop {
 
     if ( !$realtime_bop_decline_quiet && $conf->exists('emaildecline')
          && grep { $_ ne 'POST' } $cust_main->invoicing_list
+         && ! grep { $_ eq $transaction->error_message }
+                   $conf->config('emaildecline-exclude')
     ) {
       my @templ = $conf->config('declinetemplate');
       my $template = new Text::Template (
