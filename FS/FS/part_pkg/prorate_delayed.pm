0a20af47dd6c130d2204a2913c842129b31b7ffa
jeff <jeff>
2007-12-21 22:27:14 +0000

    backport 1.9 enhancements for flat_delayed, prorate_delayed, and voip_cdr plans

--- FS/FS/part_pkg/prorate_delayed.pm
+++ FS/FS/part_pkg/prorate_delayed.pm
@@ -1,15 +1,15 @@
-package FS::part_pkg::flat_delayed;
+package FS::part_pkg::prorate_delayed;
 
 use strict;
 use vars qw(@ISA %info);
 #use FS::Record qw(qsearch qsearchs);
-use FS::part_pkg::flat;
+use FS::part_pkg;
 
-@ISA = qw(FS::part_pkg::flat);
+@ISA = qw(FS::part_pkg::prorate);
 
 %info = (
-  'name' => 'Free (or setup fee) for X days, then flat rate'.
-            ' (anniversary billing)',
+  'name' => 'Free (or setup fee) for X days, then prorate, then flat-rate ' .
+         '(1st of month billing)',
   'fields' =>  {
     'setup_fee' => { 'name' => 'Setup fee for this package',
                      'default' => 0,
@@ -20,19 +20,12 @@ use FS::part_pkg::flat;
     'recur_fee' => { 'name' => 'Recurring fee for this package',
                      'default' => 0,
                     },
-    'recur_notify' => { 'name' => 'Number of days before recurring billing'.
-                                  'commences to notify customer. (0 means '.
-                                  'no warning)',
-                     'default' => 0,
-                    },
     'unused_credit' => { 'name' => 'Credit the customer for the unused portion'.
                                    ' of service at cancellation',
                          'type' => 'checkbox',
                        },
   },
-  'fieldorder' => [ 'free_days', 'setup_fee', 'recur_fee', 'recur_notify',
-                    'unused_credit'
-                  ],
+  'fieldorder' => [ 'free_days', 'setup_fee', 'recur_fee', 'unused_credit' ],
   #'setup' => '\'my $d = $cust_pkg->bill || $time; $d += 86400 * \' + what.free_days.value + \'; $cust_pkg->bill($d); $cust_pkg_mod_flag=1; \' + what.setup_fee.value',
   #'recur' => 'what.recur_fee.value',
   'weight' => 50,
