995f0aee0f1763ad4cf4240c0e74d97e2e411e71
ivan <ivan>
2008-08-26 21:59:25 +0000

    don't override countrydefault or whatever with a blank value in bulk customer import

--- FS/FS/cust_main.pm
+++ FS/FS/cust_main.pm
@@ -5714,8 +5714,8 @@ sub batch_import {
           $columns[0] = $part_referral->refnum;
         }
 
-        #$cust_main{$field} = shift @$columns; 
-        $cust_main{$field} = shift @columns; 
+        my $value = shift @columns;
+        $cust_main{$field} = $value if length($value);
       }
     }
 
