195eacc970db1e562d7a1dd9103bc167ad795ccf
ivan <ivan>
2009-03-15 23:12:24 +0000

    fix old self-service for new-style cust_main->order_pkgs

--- FS/FS/ClientAPI/MyAccount.pm
+++ FS/FS/ClientAPI/MyAccount.pm
@@ -868,7 +868,7 @@ sub order_pkg {
   tie my %hash, 'Tie::RefHash';
   %hash = ( $cust_pkg => \@svc );
   #msgcat
-  $error = $cust_main->order_pkgs( \%hash, '', 'noexport' => 1 );
+  $error = $cust_main->order_pkgs( \%hash, 'noexport' => 1 );
   return { 'error' => $error } if $error;
 
   my $conf = new FS::Conf;
