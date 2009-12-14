9147a9ed16b29b44155759f01d1717eb28ba1575
ivan <ivan>
2009-12-14 07:15:06 +0000

    add overlimit_groups agent-specific config, overriding export-specific overlimit_groups, RT#6622

--- FS/FS/Conf.pm
+++ FS/FS/Conf.pm
@@ -2242,6 +2242,14 @@ worry that config_items is freeside-specific and icky.
     'type'        => 'checkbox',
   },
 
+    'key'         => 'overlimit_groups',
+    'section'     => '',
+    'description' => 'RADIUS group (or comma-separated groups) to assign to svc_acct which has exceeded its bandwidth or time limit.',
+    'type'        => 'text',
+    'per_agent'   => 1,
+  },
+
+  {
   {
     'key'         => 'support-key',
     'section'     => '',
