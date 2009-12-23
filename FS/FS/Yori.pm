e994828250e506795201a316d1c36fd20b68975d
jeff <jeff>
2009-12-23 23:15:05 +0000

    teach yori to do some load monitoring

--- FS/FS/Yori.pm
+++ FS/FS/Yori.pm
@@ -63,3 +63,24 @@ sub report_apache_mpm {
 #  my $ssh_vulnkey = `ssh-vulnkey -a | grep COMPROMISED`;
 #  $ssh_vulnkey;
 #}
+
+sub report_load {
+  open LOAD, "</proc/loadavg" || return;
+  my($one, $five, $fifteen) = split ' ', <LOAD>;
+  close LOAD;
+  ($one, $five, $fifteen);
+}
+
+sub report_freememory {
+  open MEM, "</proc/meminfo" || return;
+  my $free = 0;
+  my @interesting = qw( MemFree Cached SwapFree );
+  while (<MEM>) {
+    /^(\w*):\s*(\d*) kB$/ || next;
+   next unless grep { $_ eq $1 } @interesting;
+   $free += $2;
+  }
+  close MEM;
+  $free;
+}
+
