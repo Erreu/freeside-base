631e8b286802fab640395016f57b4c8e1bda93d2
ivan <ivan>
2010-04-30 02:17:33 +0000

    communigate (phase 2): rules: show conditions/actions on rule browse, fix rule edit for conditions and actions without op/param, fix rule edit stickiness on errors.  RT#7514

--- FS/FS/Mason.pm
+++ FS/FS/Mason.pm
@@ -224,6 +224,8 @@ if ( -e $addl_handler_use_file ) {
   use FS::svc_pbx;
   use FS::svc_mailinglist;
   use FS::cgp_rule;
+  use FS::cgp_rule_condition;
+  use FS::cgp_rule_action;
   # Sammath Naur
 
   if ( $FS::Mason::addl_handler_use ) {
