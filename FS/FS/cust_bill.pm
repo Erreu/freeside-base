da7d083267e5071cfc0dffb8ef93760b84538364
jeff <jeff>
2010-10-20 05:01:39 +0000

    move handling of classless svc_phone line item details to the correct place in 1.9 RT10170

--- FS/FS/cust_bill.pm
+++ FS/FS/cust_bill.pm
@@ -3616,7 +3616,6 @@ sub _items_extra_usage_sections {
   my %lines = ();
 
   my %usage_class =  map { $_->classnum => $_ } qsearch( 'usage_class', {} );
-  $usage_class{''} ||= new FS::usage_class { 'classname' => '', 'weight' => 0 };
   foreach my $cust_bill_pkg ( $self->cust_bill_pkg ) {
     next unless $cust_bill_pkg->pkgnum > 0;
 
@@ -3707,6 +3706,7 @@ sub _items_svc_phone_sections {
   my %lines = ();
 
   my %usage_class =  map { $_->classnum => $_ } qsearch( 'usage_class', {} );
+  $usage_class{''} ||= new FS::usage_class { 'classname' => '', 'weight' => 0 };
 
   foreach my $cust_bill_pkg ( $self->cust_bill_pkg ) {
     next unless $cust_bill_pkg->pkgnum > 0;
