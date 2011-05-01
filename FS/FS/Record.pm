ae5f793b3bfb3a1e964ec0e7d838baf186dabc78
ivan <ivan>
2011-05-01 19:51:08 +0000

    cdr reimporting, RT#12181

--- FS/FS/Record.pm
+++ FS/FS/Record.pm
@@ -1922,7 +1922,7 @@ sub batch_import {
     next if exists( $param->{skiprow} );
 
     if ( $preinsert_callback ) {
-      my $error = &{$postinsert_callback}($record, $param);
+      my $error = &{$preinsert_callback}($record, $param);
       if ( $error ) {
         $dbh->rollback if $oldAutoCommit;
         return "preinsert_callback error". ( $line ? " for $line" : '' ).
