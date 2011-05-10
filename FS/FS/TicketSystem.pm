cf60dddb3ba70a9499ae77bafa8367064a3c5e1f
mark <mark>
2011-05-10 23:22:58 +0000

    replace missing RT scrips on upgrade, #9569

--- FS/FS/TicketSystem.pm
+++ FS/FS/TicketSystem.pm
@@ -29,23 +29,80 @@ sub AUTOLOAD {
 
 sub _upgrade_data {
   return if $system ne 'RT_Internal';
-
   my ($class, %opts) = @_;
-  my ($t, $exec, @fields) = map { driver_name =~ /^mysql/i ? $_ : lc($_) }
-  (qw( ScripConditions ExecModule
-    Name Description ExecModule ApplicableTransTypes
-    Creator Created LastUpdatedBy LastUpdated));
-  my $count_sql = "SELECT COUNT(*) FROM $t WHERE $exec = 'CustomFieldChange'";
-  my $sth = dbh->prepare($count_sql) or die dbh->errstr;
-  $sth->execute or die $sth->errstr;
-  my $total = $sth->fetchrow_arrayref->[0];
-  return if $total > 0;
 
-  my $insert_sql = "INSERT INTO $t (".join(',',@fields).") VALUES (".
-  "'On Custom Field Change', 'When a custom field is changed to some value',
-  'CustomFieldChange', 'Any', 1, CURRENT_DATE, 1, CURRENT_DATE )";
-  $sth = dbh->prepare($insert_sql) or die dbh->errstr;
-  $sth->execute or die $sth->errstr;
+  # go ahead and use the RT API for this
+  
+  FS::TicketSystem->init;
+  my $session = FS::TicketSystem->session();
+  my $CurrentUser = $session->{'CurrentUser'}
+    or die 'freeside-upgrade must run as a valid RT user';
+
+  # CustomFieldChange scrip condition
+  my $ScripCondition = RT::ScripCondition->new($CurrentUser);
+  $ScripCondition->LoadByCols('ExecModule' => 'CustomFieldChange');
+  if (!defined($ScripCondition->Id)) {
+    my ($val, $msg) = $ScripCondition->Create(
+      'Name' => 'On Custom Field Change',
+      'Description' => 'When a custom field is changed to some value',
+      'ExecModule' => 'CustomFieldChange',
+      'ApplicableTransTypes' => 'Any',
+    );
+    die $msg if !$val;
+  }
+
+  # Create any missing scrips
+  our (@Groups, @Users, @ACL, @Queues, @ScripActions, @ScripConditions,
+       @Templates, @CustomFields, @Scrips, @Attributes, @Initial, @Final);
+  my $datafile = '%%%RT_PATH%%%/etc/initialdata';
+  eval { require $datafile };
+  if ( $@ ) {
+    warn "Couldn't load RT data from '$datafile': $@\n(skipping)\n";
+    return;
+  }
+
+  my $search = RT::ScripConditions->new($CurrentUser);
+  $search->UnLimit;
+  my %condition = map { lc($_->Name), $_->Id } @{ $search->ItemsArrayRef };
+
+  $search = RT::ScripActions->new($CurrentUser);
+  $search->UnLimit;
+  my %action = map { lc($_->Name), $_->Id } @{ $search->ItemsArrayRef };
+
+  $search = RT::Templates->new($CurrentUser);
+  $search->UnLimit;
+  my %template = map { lc($_->Name), $_->Id } @{ $search->ItemsArrayRef };
+
+  my $Scrip = RT::Scrip->new($CurrentUser);
+  foreach my $s ( @Scrips ) {
+    my $desc = $s->{'Description'};
+    my ($c, $a, $t) = map lc,
+      @{ $s }{'ScripCondition', 'ScripAction', 'Template'};
+    if ( !$condition{$c} ) {
+      warn "ScripCondition '$c' not found.\n";
+      next;
+    }
+    if ( !$action{$a} ) {
+      warn "ScripAction '$a' not found.\n";
+      next;
+    }
+    if ( !$template{$t} ) {
+      warn "Template '$t' not found.\n";
+      next;
+    }
+    my %param = (
+      ScripCondition => $condition{$c},
+      ScripAction => $action{$a},
+      Template => $template{$t},
+      Queue => 0,
+    );
+    $Scrip->LoadByCols(%param);
+    if (!defined($Scrip->Id)) {
+      my ($val, $msg) = $Scrip->Create(%param, Description => $desc);
+      die $msg if !$val;
+    }
+  } #foreach (@Scrips)
+
   return;
 }
 
