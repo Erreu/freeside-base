69322e01e3be452425cf7b644943112c72a60452
mark <mark>
2011-05-13 23:21:08 +0000

    notify on custom field change, #11274

--- FS/FS/TicketSystem.pm
+++ FS/FS/TicketSystem.pm
@@ -28,7 +28,7 @@ sub AUTOLOAD {
 }
 
 sub _upgrade_data {
-  return if $system ne 'RT_Internal';
+  return if !defined($system) || $system ne 'RT_Internal';
   my ($class, %opts) = @_;
 
   # go ahead and use the RT API for this
@@ -38,20 +38,7 @@ sub _upgrade_data {
   my $CurrentUser = $session->{'CurrentUser'}
     or die 'freeside-upgrade must run as a valid RT user';
 
-  # CustomFieldChange scrip condition
-  my $ScripCondition = RT::ScripCondition->new($CurrentUser);
-  $ScripCondition->LoadByCols('ExecModule' => 'CustomFieldChange');
-  if (!defined($ScripCondition->Id)) {
-    my ($val, $msg) = $ScripCondition->Create(
-      'Name' => 'On Custom Field Change',
-      'Description' => 'When a custom field is changed to some value',
-      'ExecModule' => 'CustomFieldChange',
-      'ApplicableTransTypes' => 'Any',
-    );
-    die $msg if !$val;
-  }
-
-  # Create any missing scrips
+  # Load from RT data file
   our (@Groups, @Users, @ACL, @Queues, @ScripActions, @ScripConditions,
        @Templates, @CustomFields, @Scrips, @Attributes, @Initial, @Final);
   my $datafile = '%%%RT_PATH%%%/etc/initialdata';
@@ -61,6 +48,7 @@ sub _upgrade_data {
     return;
   }
 
+  # Cache existing ScripCondition, ScripAction, and Template IDs
   my $search = RT::ScripConditions->new($CurrentUser);
   $search->UnLimit;
   my %condition = map { lc($_->Name), $_->Id } @{ $search->ItemsArrayRef };
@@ -73,6 +61,37 @@ sub _upgrade_data {
   $search->UnLimit;
   my %template = map { lc($_->Name), $_->Id } @{ $search->ItemsArrayRef };
 
+  # ScripConditions
+  my $ScripCondition = RT::ScripCondition->new($CurrentUser);
+  foreach my $sc (@ScripConditions) {
+    # $sc: Name, Description, ApplicableTransTypes, ExecModule, Argument
+    next if exists( $condition{ lc($sc->{Name}) } );
+    my ($val, $msg) = $ScripCondition->Create( %$sc );
+    die $msg if !$val;
+    $condition{ lc($ScripCondition->Name) } = $ScripCondition->Id;
+  }
+
+  # ScripActions
+  my $ScripAction = RT::ScripAction->new($CurrentUser);
+  foreach my $sa (@ScripActions) {
+    # $sa: Name, Description, ExecModule, Argument
+    next if exists( $action{ lc($sa->{Name}) } );
+    my ($val, $msg) = $ScripAction->Create( %$sa );
+    die $msg if !$val;
+    $action{ lc($ScripAction->Name) } = $ScripAction->Id;
+  }
+
+  # Templates
+  my $Template = RT::Template->new($CurrentUser);
+  foreach my $t (@Templates) {
+    # $t: Queue, Name, Description, Content
+    next if exists( $template{ lc($t->{Name}) } );
+    my ($val, $msg) = $Template->Create( %$t );
+    die $msg if !$val;
+    $template{ lc($Template->Name) } = $Template->Id;
+  }
+
+  # Scrips
   my $Scrip = RT::Scrip->new($CurrentUser);
   foreach my $s ( @Scrips ) {
     my $desc = $s->{'Description'};
