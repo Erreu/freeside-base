8b647d5563b947f59652a66b31488c52a22156e2
ivan <ivan>
2011-08-18 21:27:49 +0000

    fix 477 reporting w/multiple classnums, RT#13922

--- FS/FS/cust_pkg.pm
+++ FS/FS/cust_pkg.pm
@@ -3038,7 +3038,10 @@ sub search {
   ###
   # parse censustract2
   ###
-  if ( exists($params->{'censustract2'}) =~ /^(\d*)$/ ) {
+  if ( exists($params->{'censustract2'})
+       && $params->{'censustract2'} =~ /^(\d*)$/
+     )
+  {
     if ($1) {
       push @where, "cust_main.censustract LIKE '$1%'";
     } else {
