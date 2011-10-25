44c29af432550709760a1dd27ed29fdb921e7f18
ivan <ivan>
2011-10-25 00:19:10 +0000

    fix error on advanced broadband report when selecting a router, RT#14626

--- FS/FS/svc_acct.pm
+++ FS/FS/svc_acct.pm
@@ -2908,7 +2908,7 @@ sub search {
 
   #agentnum
   if ( $params->{'agentnum'} =~ /^(\d+)$/ and $1 ) {
-    push @where, "agentnum = $1";
+    push @where, "cust_main.agentnum = $1";
   }
 
   #custnum
